f340e80cd101dace3dbd79dd319fd7c7f8010716
SAURABH <saurabh44gupta@gmail.com>
2019-11-10 22:54:45 +0530

    add

--- Tprimes.cpp
+++ Tprimes.cpp
@@ -3,42 +3,61 @@
 //
 
 #include<iostream>
+#include<bits/stdc++.h>
 
 using namespace std;
 bool checkTprime(long long n);
+#define LIMIT 1000000
+long long primes[LIMIT];
+
+void allPrimes();
+int check_perfect_square(long long n);
 
 int main(){
 
+    allPrimes();
     long long n;
     cin>>n;
 
     while(n-->0){
         long long temp;
         cin>>temp;
-        bool flag=false;
-        flag=checkTprime(temp);
-
-        if(flag){
+        if(temp==4){
             cout<<"YES"<<endl;
 
+        }else if(temp%2==0){
+            cout<<"NO"<<endl;
+        }else if(check_perfect_square(temp)==1 && primes[int(sqrt(temp))]==0){
+            cout<<"YES"<<endl;
         }else{
             cout<<"NO"<<endl;
         }
     }
 }
 
-bool checkTprime(long long n){
+ void allPrimes(){
 
-    int count=0;
+    primes[0]=1;
+    primes[1]=1;
 
-    for(long long i=2;i<=n/2;i++){
+    for(int i=2;i<=sqrt(LIMIT);i++){
 
-        if(n%i==0){
-            count++;
+        if(primes[i]==0){
+
+            for(int j=i;i*j<LIMIT;j++){
+                primes[i*j]=1;
+            }
         }
-
-        if(count>1) return false;
     }
-    if(count==0) return false;
-    return true;
+}
+
+int check_perfect_square(long long n){
+    double sqrt_n = sqrt(n);
+
+    if(sqrt_n == int(sqrt_n)){
+        return 1;
+    }
+    else{
+        return 0;
+    }
 }
