7d30f0fca3709b12f81d7568f74a729c9e9691df
Saurabh Gupta <saurabh44gupta@gmail.com>
2019-11-23 07:51:06 +0530

    changes

--- Buttons.cpp
+++ Buttons.cpp
@@ -2,6 +2,8 @@
 // Created by Saurabh on 11/21/2019.
 //
 
+//<a href="https://www.codeforces.com";
+
 #include<iostream>
 
 using namespace std;
---
+++ SerejaAndSuffixes.cpp
@@ -0,0 +1,32 @@
+//
+// Created by Saurabh on 11/20/2019.
+//
+
+#include<iostream>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+int main(){
+
+    int n,k;
+    cin>>n>>k;
+
+    vector<int> list;
+
+    while(n-->0){
+        int a;
+        cin>>a;
+        list.push_back(a);
+    }
+
+    set<int> s;
+
+    while(k-->0){
+        int a;
+        cin>>a;
+        s.insert()
+    }
+}
+
