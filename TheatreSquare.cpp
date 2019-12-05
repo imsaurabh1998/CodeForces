b8fdc1a7fb36b63f09c1e4a595e82b63d49880cd
Saurabh Gupta <saurabh44gupta@gmail.com>
2019-12-05 11:55:28 +0530

    add

---
+++ TheatreSquare.cpp
@@ -0,0 +1,39 @@
+//
+// Created by Saurabh on 12/5/2019.
+//
+//
+
+
+#include<iostream>
+
+
+using namespace std;
+
+int main() {
+
+    int n,m,a;
+    cin >> n>>m>>a;
+
+    if (n == m && m == a) {
+        cout << 1 << endl;
+        return 0;
+    }
+
+    long long int totalWidthFlag = (n % a == 0) ? n / a : (n / a) + 1;
+    m -= a;
+    long long int totalLengthFlag = 0;
+    if(m>0)
+        totalLengthFlag = (m <= a) ? totalWidthFlag : (m > a) ? (m  / a * totalWidthFlag + ((m) % a == 0 ? 0 : totalWidthFlag)) : 0;
+
+    cout << totalWidthFlag + totalLengthFlag << endl;
+
+
+
+
+
+
+
+
+
+}
+
