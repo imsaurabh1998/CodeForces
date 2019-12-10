50cfd82fe738f89b18f64963160deab2406786d7
Saurabh Gupta <saurabh44gupta@gmail.com>
2019-12-10 18:23:53 +0530

    add

--- SoldierAndBadges.cpp
+++ SoldierAndBadges.cpp
@@ -10,41 +10,64 @@
 
 using namespace std;
 
-int main(){
+int main() {
 
     int n;
+    cin >> n;
     vector<int> list;
-    map<int ,int> m;
-    int mn=INT32_MIN;
-    int mx=INT32_MAX;
-    while(n-->0){
+    map<int, int> m;
+    int mx = INT32_MIN;
+    int mn = INT32_MAX;
+    while (n-- > 0) {
         int temp;
-        cin>>temp;
+        cin >> temp;
         list.push_back(temp);
         m[temp]++;
-        mn=min(mn,temp);
-        mx=max(mx,temp);
+        mn = min(mn, temp);
+        mx = max(mx, temp);
     }
 
-    sort(list.begin(),list.end());
 
-    int minCount=0;
+    sort(list.begin(), list.end());
 
-    for(int i=0;i<list.size()-1;i++){
+    int minCount = 0;
 
-        if(list[i]==list[i+1]){
+    /*for (int i = 0; i < list.size() - 1; i++) {
 
-            while(m[mn]!=0){
+        if (list[i] == list[i + 1]) {
+
+            while (true) {
+                if (m[mn] == 0 && list[i + 1] < mn) {
+                    break;
+                }
                 mn++;
             }
-            minCount+=mn-list[i+1];
-            list[i+1]=mn++;
+            minCount += mn - list[i + 1];
+            list[i + 1] = mn++;
 
         }
+    }*/
+
+    for (auto a : m) {
+        int first = a.first;
+        int second = a.second;
+
+        while (second-- > 1) {
+
+            while (true) {
+                if (m[mn] == 0 && a.first < mn) {
+                    minCount += mn - a.first;
+                    mn++;
+                    break;
+                }
+                mn++;
+            }
+        }
     }
+    cout << minCount << endl;
 
-    cout<<minCount<<endl;
-
+    /*13
+        1 1 2 2 4 5 11 13 14 17 18 18 18*/
 
 
 }
