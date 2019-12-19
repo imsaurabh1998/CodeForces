d390741d533b0a767705b36d3b7238727b5d7c0c
SAURABH <saurabh44gupta@gmail.com>
2019-12-19 22:12:33 +0530

    Merge branch 'master' of https://github.com/imsaurabh1998/CodeForces
     Conflicts:
    	CutRibbon.cpp

---
+++ AandB.cpp
@@ -0,0 +1,50 @@
+//
+// Created by SAURABH on 19-12-2019.
+//
+
+#include<bits/stdc++.h>
+
+typedef  long long int ll;
+
+using namespace std;
+
+int main(){
+
+    int n;
+    cin>>n;
+    while(n-->0){
+
+        ll a,b;
+        cin>>a>>b;
+
+        if(a==b){
+            cout<<0<<endl;
+        }else {
+            if(a>b){
+                swap(a,b);
+            }
+            ll count=0;
+            ll i=1;
+
+            while(true){
+
+                if(a+i<=b){
+                    a+=i++;
+                    count++;
+                }else break;
+
+            }
+
+            while(a!=b){
+
+                b+=i++;
+                a+=i++;
+                count+=2;
+
+            }
+
+            cout<<count<<endl;
+        }
+    }
+}
+
