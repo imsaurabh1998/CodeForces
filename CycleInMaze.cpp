759074d0338525144178b1dc887dd3ce3adf3470
SAURABH <saurabh44gupta@gmail.com>
2020-01-21 21:45:56 +0530

    Merge branch 'master' of https://github.com/imsaurabh1998/CodeForces
     Conflicts:
    	CutRibbon.cpp

--- CycleInMaze.cpp
+++ CycleInMaze.cpp
@@ -7,80 +7,86 @@
 using namespace std;
 #define pi pair<int,int>
 
-int d[10005][10005];
-char s[10005][10005];
-string dir[]={"D","U","L","R"};
-int fx[]={0,1,0,-1};
-int fy[]={1,0,-1,0};
+int d[1005][1005];
+char s[1005][1005];
+string dir[]{ "D","U","L","R" };
+int fx[] = { 1,-1,0,0 };
+int fy[] = { 0,0,-1,1 };
 
-void dfs(int x,int y){
-    memset(d,-1,sizeof(d));
-    int sx=x;
-    int sy=y;
+void dfs(int x, int y) {
+    memset(d, -1, sizeof(d));
+    int sx = x;
+    int sy = y;
     queue<pi> q;
-    q.push({x,y});
+    q.push({ x,y });
 
-    while(!q.empty()){
+    while (!q.empty()) {
 
-        pi tp=q.front();
+        pi tp = q.front();
         q.pop();
-        for(int i=0;i<4;i++){
+        for (int i = 0; i < 4; i++) {
 
-            int x=fx[i]+tp.first;
-            int y=fy[i]+tp.second;
+            int x = fx[i] + tp.first;
+            int y = fy[i] + tp.second;
 
-            if(s[x][y]=='.' && d[x][y]==-1){
-                d[x][y]=d[tp.first][tp.second]+1;
-                q.push({x,y});
+            if (x>-1 && y>-1 && s[x][y] == '.' && d[x][y] == -1) {
+                d[x][y] = d[tp.first][tp.second] + 1;
+                q.push({ x,y });
             }
         }
     }
 }
 
 
-int main(){
+int main() {
 
-    int n,m,k;
+    int n, m, k;
+    cin >> n >> m >> k;
     char temp;
-    int Sx,Sy;
-    for(int i=0;i<n;i++){
+    int Sx=0, Sy=0;
+    for (int i = 0; i < n; i++) {
 
-        for(int j=0;j<m;j++){
-            cin>>temp;
+        for (int j = 0; j < m; j++) {
+            cin >> temp;
 
-            if(temp=='X') {
-                Sx=i;
-                Sy=j;
-                s[i][j]='.';
+            if (temp == 'X') {
+                Sx = i;
+                Sy = j;
+                s[i][j] = '.';
             }
-            cin>>s[i][j];
+            else {
+                s[i][j]=temp;
+            }
 
         }
     }
 
-    dfs(Sx,Sy);
+    dfs(Sx, Sy);
 
-    if(k&1) cout<<"IMPOSSIBLE\n";
-    else{
+    if (k & 1) cout << "IMPOSSIBLE\n";
+    else {
         string ss;
-        while(k){
-            int f=0;
-            for(int i=0;i<4;i++){
-                int x=fx[i]+Sx;
-                int y=fy[i]+Sy;
-                if(s[x][y]=='.' && d[x][y]<=k){
-                    Sx=x; Sy=y;
-                    ss+=dir[i];
-                    f=1;
+        while (k) {
+            int f = 0;
+            for (int i = 0; i < 4; i++) {
+                int x = fx[i] + Sx;
+                int y = fy[i] + Sy;
+                if (s[x][y] == '.' && d[x][y] <= k) {
+                    Sx = x; Sy = y;
+                    ss += dir[i];
+                    f = 1;
                     break;
                 }
 
             }
 
-            if(!f) cout<<"IMPOSSIBLE\n";
+            if (!f) {
+                cout << "IMPOSSIBLE\n";
+                return 0;
+            }
             k--;
         }
-        cout<<ss<<endl;
+        cout << ss << endl;
     }
 
 }
