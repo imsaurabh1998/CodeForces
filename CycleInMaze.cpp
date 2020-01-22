4fa1dc4390cab010abd1eff6ed318beb9a4b568e
Saurabh Gupta <saurabh44gupta@gmail.com>
2020-01-22 11:14:42 +0530

    Merge branch 'master' of https://github.com/imsaurabh1998/CodeForces
     Conflicts:
    	CycleInMaze.cpp

--- CycleInMaze.cpp
+++ CycleInMaze.cpp
@@ -7,16 +7,17 @@
 using namespace std;
 #define pi pair<int,int>
 
-int d[10005][10005];
-char s[10005][10005];
-string dir[]={"D","U","L","R"};
-int fx[]={0,1,0,-1};
-int fy[]={1,0,-1,0};
+int d[1005][1005];
+char s[1005][1005];
+char dir[4] = { 'D','L','R','U' };
+int fx[] = { 1,0,0,-1 };
+int fy[] = { 0,-1,1,0 };
 
 void dfs(int x,int y){
     memset(d,-1,sizeof(d));
     int sx=x;
     int sy=y;
+    d[x][y]=0;
     queue<pi> q;
     q.push({x,y});
 
@@ -29,7 +30,7 @@ void dfs(int x,int y){
             int x=fx[i]+tp.first;
             int y=fy[i]+tp.second;
 
-            if(s[x][y]=='.' && d[x][y]==-1){
+            if(x>-1 && y>-1 && s[x][y]=='.' && d[x][y]==-1){
                 d[x][y]=d[tp.first][tp.second]+1;
                 q.push({x,y});
             }
@@ -41,6 +42,8 @@ void dfs(int x,int y){
 int main(){
 
     int n,m,k;
+    cin>>n>>m>>k;
+
     char temp;
     int Sx,Sy;
     for(int i=0;i<n;i++){
@@ -53,22 +56,26 @@ int main(){
                 Sy=j;
                 s[i][j]='.';
             }
-            cin>>s[i][j];
+           else  s[i][j]=temp;
 
         }
     }
 
-    dfs(Sx,Sy);
 
-    if(k&1) cout<<"IMPOSSIBLE\n";
-    else{
+
+    if(k&1) {
+        cout << "IMPOSSIBLE\n";
+        return 0;
+    }
+
+    dfs(Sx,Sy);
         string ss;
         while(k){
             int f=0;
             for(int i=0;i<4;i++){
                 int x=fx[i]+Sx;
                 int y=fy[i]+Sy;
-                if(s[x][y]=='.' && d[x][y]<=k){
+                if(x>-1 && y>-1 && s[x][y]=='.' && d[x][y]<=k){
                     Sx=x; Sy=y;
                     ss+=dir[i];
                     f=1;
@@ -77,10 +84,13 @@ int main(){
 
             }
 
-            if(!f) cout<<"IMPOSSIBLE\n";
+            if(!f) {
+                cout << "IMPOSSIBLE\n";
+                return 0;
+            }
             k--;
         }
         cout<<ss<<endl;
-    }
+
 
 }
