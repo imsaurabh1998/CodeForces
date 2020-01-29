5084c2fb15e9da7d652e9423e1e36fcbd41fa58c
SAURABH <saurabh44gupta@gmail.com>
2020-01-29 21:27:09 +0530

    Merge branch 'master' of https://github.com/imsaurabh1998/CodeForces
     Conflicts:
    	CutRibbon.cpp

---
+++ InfinitePrefix.cpp
@@ -0,0 +1,33 @@
+//
+// Created by SAURABH on 29-01-2020.
+//
+
+#include<bits/stdc++.h>
+typedef long long int ll;
+using namespace std;
+
+int main(){
+
+    int n;
+    cin>>n;
+
+    while(n-->0){
+
+        ll n,x;
+        cin>>n>>x;
+
+        string val;
+        cin>>val;
+        bool flag=false;
+
+        ll zero, one;
+        for( ll i =0;i<val.length();i++){
+
+            if(val[i]=='0') zero++;
+            else one++;
+        }
+
+
+
+    }
+}
