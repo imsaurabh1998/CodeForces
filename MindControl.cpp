263dfd47eaee990d64b72e091cdf206d31fa43e3
SAURABH <saurabh44gupta@gmail.com>
2020-02-03 22:19:57 +0530

    add

---
+++ MindControl.cpp
@@ -0,0 +1,24 @@
+//
+// Created by SAURABH on 03-02-2020.
+//
+#include<bits/stdc++.h>
+
+using namespace std;
+int arr[4000];
+int main(){
+
+    int n;
+    cin>>n;
+
+    while(n-->0){
+
+        int n,m,k;
+        cin>>n>>m>>k;
+
+        for(int i=0;i<n;i++) cin>>arr[i];
+
+        int start=0, end=n-1;
+        for(int i=0;i<m;i++) start
+    }
+}
+
